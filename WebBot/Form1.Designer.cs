bde65e8137b81ab206666f68cc9ee88ca649bfd9
Sergeim <smiroshnikov@securitydam.com>
2018-09-02 21:32:50 +0300

    added TODO

--- WebBot/Form1.Designer.cs
+++ WebBot/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 78);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 48);
             this.button1.TabIndex = 0;
@@ -44,19 +45,30 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 147);
+            this.button2.Location = new System.Drawing.Point(141, 167);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 51);
             this.button2.TabIndex = 1;
             this.button2.Text = "Exit Browser";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Фотки Луны на Яндексе";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 230);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
